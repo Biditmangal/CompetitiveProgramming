60531228e5cb97458af5d804bfd952362c9f341d
Biditmangal <biditmangal9506@gmail.com>
2021-01-04 19:39:58 +0530

    Completed four problems of D-1000

---
+++ CodeForces/D-1000/1373A.cpp
@@ -0,0 +1,63 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define pii pair<int,int>
+#define MOD 1000000007
+
+
+void solve(){
+    
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll ans1=-1, ans2=-1;
+
+    if(a<c){
+        ans1=1;
+    }
+    if(a*b > c){
+        ans2=b;
+    }
+
+    cout<<ans1<<" "<<ans2<<endl;
+
+}
+
+int main(){
+
+    int t;
+    cin>>t;
+    // t=1;
+    
+    while(t--) solve();
+
+    return 0;
+}
