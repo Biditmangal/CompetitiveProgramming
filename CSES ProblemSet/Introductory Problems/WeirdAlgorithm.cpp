ea6d5e3e79bb97c0e1ebc5dab16a13438b6c908f
Biditmangal <biditmangal9506@gmail.com>
2021-01-05 23:21:46 +0530

    Completed 4 problems of CSES Introductory problems

---
+++ CSES ProblemSet/Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,64 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define pii pair<int,int>
+#define MOD 1000000007
+
+void solve(){
+    ll n;
+    cin>>n;
+
+    while(n!=1 && n>0){
+        cout<<n<<" ";
+        if(n%2!=0){
+            n=n*3+1;
+        }
+        else{
+            n=n/2;
+        }
+    }
+    cout<<n;
+
+}
+
+int main(){
+
+    int t;
+    // cin>>t;
+    t=1;
+    
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
