fd19d7dde4767081d7a0ed7687c42fd6e01fd49a
Biditmangal <biditmangal9506@gmail.com>
2021-01-16 23:19:46 +0530

    Completed 3 D-1100 problems

---
+++ CodeForces/D-1100/1382B.cpp
@@ -0,0 +1,63 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+const int mxN = 2e5;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int odd = 0,k=1;
+    int temp;
+    FOR(i,0,n){
+        odd^=k;
+        cin>>temp;
+        if(temp>1){
+            k=0;
+        }    
+    }
+    cout<<(odd ? "First" : "Second")<<endl;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
