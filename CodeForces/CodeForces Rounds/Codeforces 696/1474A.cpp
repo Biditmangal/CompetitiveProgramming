6f90981a9119031e1cdadc7fd9f235e237dd3045
Biditmangal <biditmangal9506@gmail.com>
2021-01-19 22:35:29 +0530

    Completed 2 problems

---
+++ CodeForces/CodeForces Rounds/Codeforces 696/1474A.cpp
@@ -0,0 +1,72 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+const int mxN = 2e5;
+
+void solve()
+{
+	int n;
+	cin>>n;
+	char b[n];
+	
+	FOR(i,0,n){
+		cin>>b[i];
+	}
+	int k=0;
+	FOR(i,0,n){
+		if(b[i]+1 != k){
+			cout<<1;
+			k=b[i]+1;
+		}
+		else{
+			cout<<0;
+			k=b[i];
+		}
+	}
+	cout<<endl;
+
+
+}
+int main()
+{
+	int t = 1;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
