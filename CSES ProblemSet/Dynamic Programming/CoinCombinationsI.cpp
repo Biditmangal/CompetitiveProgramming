f2295dda182accc581e2ef1505b8797220c2bc43
Biditmangal <biditmangal9506@gmail.com>
2021-01-23 23:25:01 +0530

    Completed CoinCombinations1 from CSES DP section

---
+++ CSES ProblemSet/Dynamic Programming/CoinCombinationsI.cpp
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+
+    FOR(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> dp(x + 1, 0);
+
+    dp[0] = 1;
+
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i >= a[j])
+                dp[i] = (dp[i] + dp[i - a[j]]) % MOD;
+        }
+    }
+    cout << dp[x] << endl;
+}
+int main()
+{
+    int t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
