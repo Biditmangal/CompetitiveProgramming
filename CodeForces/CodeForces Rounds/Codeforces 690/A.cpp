bdc20ef14a7e42ec7baacabe32f215fc96e3b1ab
Biditmangal <biditmangal9506@gmail.com>
2021-01-26 20:15:51 +0530

    Did Codeforces 690

---
+++ CodeForces/CodeForces Rounds/Codeforces 690/A.cpp
@@ -0,0 +1,85 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin >> n;
+    ll a[n + 1];
+
+    FOR(i, 1, n + 1)
+    {
+        cin >> a[i];
+    }
+
+    ll ans[n];
+
+    int i = 1, j = n;
+    for (int k = 0; k < n; k++)
+    {
+
+        if (k % 2 == 0)
+        {
+            ans[k] = a[i];
+            i++;
+        }
+        else
+        {
+            ans[k] = a[j];
+            j--;
+        }
+    }
+
+    for (ll &v : ans)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
