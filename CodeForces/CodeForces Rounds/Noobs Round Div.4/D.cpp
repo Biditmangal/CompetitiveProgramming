49df8d4d3eb42097426cb1d11a000c6097f47117
Biditmangal <biditmangal9506@gmail.com>
2021-01-27 22:15:55 +0530

    Participated In my first GYM round of Codeforces

---
+++ CodeForces/CodeForces Rounds/Noobs Round Div.4/D.cpp
@@ -0,0 +1,84 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+
+    int scoreA=a^b;
+    int scoreB=0;
+
+    if(scoreA==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        // if(a>9 || b>9){
+        //     cout<<"YES"<<endl;
+        // }
+        // else{
+        //     cout<<"NO"<<endl;
+        // }
+
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(i^j > scoreA){
+                    scoreB=i^j;
+                    break;
+                }
+            }
+        }
+        if(scoreB>scoreA){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
