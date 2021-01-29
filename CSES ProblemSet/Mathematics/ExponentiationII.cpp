ea761e3bd07a1e607105945bfefac45581a2e2e3
Biditmangal <biditmangal9506@gmail.com>
2021-01-30 00:24:23 +0530

    Solved Exponentiation II of CSES Mathematics section

---
+++ CSES ProblemSet/Mathematics/ExponentiationII.cpp
@@ -0,0 +1,72 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+const int mxN = 2e5;
+
+ll modpow(ll x, ll n, ll m)
+{
+    if (n == 0)
+        return 1 % m;
+    ll u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1)
+        u = (u * x) % m;
+    return u;
+}
+
+void solve()
+{
+    ll a, b,c;
+    cin >> a >> b >> c;
+
+    ll mid = modpow(b,c,MOD-1);
+    cout << modpow(a, mid, MOD) << "\n";
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
