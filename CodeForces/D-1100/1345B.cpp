68e28b54a36e27040e07c6e58c263e9736282bb1
Biditmangal <biditmangal9506@gmail.com>
2021-02-03 23:51:16 +0530

    Did a D-1100 problem

---
+++ CodeForces/D-1100/1345B.cpp
@@ -0,0 +1,66 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin >> n;
+    int ans = 0;
+
+    while (n > 1)
+    {   //on every turn 2 side cards and 1 base card is needed to make a pyramid so we loop 2 pointer for the same
+        for (int i = 2, j = 0; i + j <= n; i += 2, j++)
+        {
+            n -= i + j;
+        }
+        ans++;
+    }
+    cout << ans << "\n";
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
