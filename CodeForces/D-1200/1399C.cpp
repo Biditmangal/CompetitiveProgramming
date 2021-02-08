1c59d5a44084be9ce05b3ead89c275250472eff5
Biditmangal <biditmangal9506@gmail.com>
2021-02-08 15:28:51 +0530

    Did 4 D-1200 problems

---
+++ CodeForces/D-1200/1399C.cpp
@@ -0,0 +1,78 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin>>n;
+    int a[n];
+    int ans = 0 ;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+    sort(a + 1, a + 1 + n);
+    for (int i = 2 * n; i >= 2; --i)
+    {
+        int l = 1, r = n;
+        int cnt = 0;
+        while (l < r)
+        {
+            if (a[l] + a[r] > i)
+                --r;
+            else if (a[l] + a[r] == i)
+                ++cnt, ++l, --r;
+            else
+                ++l;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
