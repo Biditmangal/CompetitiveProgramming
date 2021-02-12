5d5658b8d23b45715c5d60619a58e6405ccd9518
Biditmangal <biditmangal9506@gmail.com>
2021-02-12 22:51:14 +0530

    Participated in Round 701

---
+++ CodeForces/CodeForces Rounds/Codeforces 701/1485A.cpp
@@ -0,0 +1,74 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+// #define MAXN 10000000
+const int mxA = 1e5;
+
+void solve()
+{
+    // code goes here...
+    int a, b;
+    cin >> a >> b;
+    int ans = MOD;
+    for (int i = 0; i < 20; i++)
+    {   
+        if(b+i>1){
+            int cnt = i;
+            int temp = a;
+            while (temp)
+            {
+                temp /= (b+i);
+                cnt++;
+            }
+            ans = min(ans, cnt);
+        }
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+// increase b until log(a)/log(b)-log(a)/log(b+1)>1 and then do a/b until a>0.
