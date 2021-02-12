db65dddaae7b4bbfbad8eeaf894740e64e3cb489
Biditmangal <biditmangal9506@gmail.com>
2021-02-12 22:51:29 +0530

    Solved D-1200 problem

---
+++ CodeForces/D-1200/1369B.cpp
@@ -0,0 +1,76 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string s1, s0;
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            break;
+        }
+        s0 += s[i];
+    }
+    for (int j = n - 1; j >= i; j--)
+    {
+        s1 = s[j] + s1;
+        if (s[j] == '0')
+        {
+            break;
+        }
+    }
+    cout << s0 + s1 << endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
