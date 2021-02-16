99d83bb6a1fde5c8e4d37dea920a3b16138c5b80
Biditmangal <biditmangal9506@gmail.com>
2021-02-16 22:27:16 +0530

    Participated in Round 702

---
+++ CodeForces/CodeForces Rounds/Codeforces 702/1490B.cpp
@@ -0,0 +1,96 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin >> n;
+    int ans = 0;
+    int a = 0, b = 0, c = 0;
+    FOR(i, 0, n)
+    {
+        int x;
+        cin >> x;
+        if (x % 3 == 0)
+        {
+            a++;
+        }
+        else if (x % 3 == 1)
+        {
+            b++;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    
+    int avg = n/3;
+    while(a!=b||b!=c){
+        if(a>avg){
+            a--;
+            b++;
+            ans++;
+        }
+        if (b > avg)
+        {
+            b--;
+            c++;
+            ans++;
+        }
+        if (c > avg)
+        {
+            c--;
+            a++;
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
