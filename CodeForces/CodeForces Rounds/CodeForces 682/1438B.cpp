6205306b55e5eb66c38ca7871f023af362b0bdcc
Biditmangal <biditmangal9506@gmail.com>
2021-02-17 15:12:43 +0530

    Solved some Previous contest problems

---
+++ CodeForces/CodeForces Rounds/CodeForces 682/1438B.cpp
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+void solve()
+{
+    // code goes here...
+    int n;
+    cin>>n;
+    int a[n+1];
+    FOR(i,1,n+1){
+        int x;
+        cin>>x;
+        a[i]=x;
+    }
+
+    sort(a+1,a+n+1);
+    FOR(i,1,n){
+        if(a[i]==a[i+1]){
+            cout<<"YES\n";
+            return;
+        }
+    }
+    cout<<"NO\n";
+
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
