989557b7caffd35a48c28af5911d2bef6a8ae625
Biditmangal <biditmangal9506@gmail.com>
2021-02-20 00:16:30 +0530

    Upsolved problem from Round 702

---
+++ 1490D#.cpp
@@ -0,0 +1,75 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORD(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+
+int a[101];
+
+void query(int l,int r,int d){
+    if(l>r)
+        return;
+    int m =l;
+    FOR(i,l,r+1){
+        if(a[i]>a[m]){
+            m=i;
+        }
+    }
+    query(l,m-1,d+1);
+    cout<<d<<" ";
+    query(m+1,r,d+1);
+}
+void solve()
+{
+    // code goes here...
+    int n;
+    cin>>n;
+    FOR(i,1,n+1){
+        cin>>a[i];
+    }
+    query(1,n,0);
+    cout<<endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
