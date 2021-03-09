16015b6b05dbd19138254125bbbdac2295a70b6d
Biditmangal <biditmangal9506@gmail.com>
2021-03-09 22:11:13 +0530

    Solved some problems from A2OJ ladder

---
+++ CodeForces/CodeForces ProblemSet/D-1300/236B.cpp
@@ -0,0 +1,72 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define REP(i, n) FOR(i, 0, (int)n - 1)
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define MS0(x) memset(x, 0, sizeof(x))
+#define MS1(x) memset(x, 1, sizeof(x))
+#define SORT(a, n) sort(begin(a), begin(a) + n)
+#define REV(a, n) reverse(begin(a), begin(a) + n)
+#define ll long long
+#define pii pair<int, int>
+#define MOD 1000000007
+#define INT_MAX 2147483647
+
+// const int MOd = 1073741824;
+void solve()
+{
+    //code goes here...
+    int a, b, c, s = 0;
+    cin >> a >> b >> c;
+    int ar[1000010];
+    for (int i = 1; i <= 1000000; i++)
+    {
+        for (int j = i; j <= 1000000; j += i)
+            ar[j]++;
+    }
+    for (int i = 1; i <= a; i++)
+        for (int j = 1; j <= b; j++)
+            for (int k = 1; k <= c; k++)
+            {
+                s += ar[i * j * k];
+            }
+    cout << s;
+}
+int main()
+{
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdouw);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
